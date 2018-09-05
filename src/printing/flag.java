2bcce7b8965dedcb6fbd58d8d426a60a94a6e99b
ivarnohre <ivar.nohre@elev.ga.ntig.se>
2018-09-05 14:59:03 +0200

    flag

---
+++ src/printing/flag.java
@@ -0,0 +1,27 @@
+package printing;
+
+public class flag {
+
+	public static void main(String[] args) {
+		System.out.println("  __\r\n" + 
+				"  <__>\r\n" + 
+				"   ||______________________________\r\n" + 
+				"   ||#######BBB####################|\r\n" + 
+				"   ||#######BBB####################|\r\n" + 
+				"   ||#######BBB####################|\r\n" + 
+				"   ||BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB|\r\n" + 
+				"   ||BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB| \r\n" + 
+				"   ||#######BBB####################|\r\n" + 
+				"   ||#######BBB####################|\r\n" + 
+				"   ||#######BBB####################|\r\n" + 
+				"   ||#######BBB##############JGS###|\r\n" + 
+				"   ||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n" + 
+				"   ||\r\n" + 
+				"   ||  \r\n" + 
+				"   ||\r\n" + 
+				"   ||\r\n" + 
+				" ");
+
+	}
+
+}
