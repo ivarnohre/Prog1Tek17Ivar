18723f4a9ad1c21a0d4eca32dbc0c8e11268245a
ivarnohre <ivar.nohre@elev.ga.ntig.se>
2018-09-10 11:20:45 +0200

    lullaby

---
+++ src/printing/lullabyprint.java
@@ -0,0 +1,15 @@
+package printing;
+
+public class lullabyprint {
+
+	public static void main(String[] args) {
+		System.out.println("Twinkle, twinkle, litte star,\r\n" + 
+				"   How I wonder what you are! \r\n" + 
+				"    Up above the world so high,     \r\n" + 
+				"    Like a diamond in the sky. \r\n" + 
+				"  Twinkle, twinkle, little star, \r\n" + 
+				"   How I wonder what yolu are");
+
+	}
+
+}
