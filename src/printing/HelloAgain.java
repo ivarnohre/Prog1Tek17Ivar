0f3635d98ebf9eba71d717f377d0725831f3d2ab
ivarnohre <ivar.nohre@elev.ga.ntig.se>
2018-09-10 11:18:30 +0200

    face

--- src/printing/HelloAgain.java
+++ src/printing/HelloAgain.java
@@ -3,7 +3,7 @@ package printing;
 public class HelloAgain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello again")
+		System.out.println("Hello again");
 	}
 
 }
---
+++ src/printing/face.java
@@ -0,0 +1,13 @@
+package printing;
+
+public class face {
+
+	public static void main(String[] args) {
+		System.out.println("   +\"\"\"\"\"+                                                 \r\n" + 
+				           "  [| o o |]                                                \r\n" + 
+			 	           "   |  ^  |                                                 \r\n" + 
+				           "   | '-' |                                                 \r\n" + 
+				           "   +-----+");
+	}
+
+}
