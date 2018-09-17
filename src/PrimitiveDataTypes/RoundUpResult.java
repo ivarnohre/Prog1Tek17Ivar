5179e1c6d4a203a98a390a80427ff963af7b6ae1
ivarnohre <ivar.nohre@elev.ga.ntig.se>
2018-09-17 15:03:41 +0200

    PrimitiveDataTypes

---
+++ src/PrimitiveDataTypes/RoundUpResult.java
@@ -0,0 +1,11 @@
+package PrimitiveDataTypes;
+
+public class RoundUpResult {
+
+	public static void main(String[] args) {
+		System.out.println("47 / 8 = " + 47.0 / 8);
+		System.out.println("5.875 > " + Math.round(47.0 / 8));
+
+	}
+
+}
