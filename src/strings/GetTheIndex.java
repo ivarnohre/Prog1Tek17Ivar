e514ccfa0d965ce3d33d031f4dbe3ee1f26ebade
ivarnohre <ivar.nohre@elev.ga.ntig.se>
2018-11-26 12:16:55 +0100

    Update

--- src/strings/GetTheIndex.java
+++ src/strings/GetTheIndex.java
@@ -3,11 +3,12 @@ package strings;
 import java.util.Scanner;
 
 public class GetTheIndex {
-
+    //This declares a string as the Swedish alphabet
 	public static void main(String[] args) {
 		String S = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ";
 		System.out.println("The Swedish alphabet looks like this: " + S);
 		
+		//Scans a number and uses that number in the position 
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Type a position in the alphabet between 0 and 28 to get the letter: ");
 		int pos = scanner.nextInt();
