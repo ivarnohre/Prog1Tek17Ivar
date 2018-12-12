f3379ac6dd38a0ed9ec57a92c946cd7c9ec8ee1e
ivarnohre <ivar.nohre@elev.ga.ntig.se>
2018-12-12 14:11:51 +0100

    ugly

--- src/operator/UglyNumbers.java
+++ src/operator/UglyNumbers.java
@@ -1,7 +1,11 @@
 package operator;
 
 import java.util.Scanner;
-
+ /**
+  * This program will determine ugly numbers. A ugly number is a number whos only prime factors are 2,3 or 5.
+  * @author ivar.nohre
+  *
+  */
 public class UglyNumbers {
 
 	public static void main(String[] args) {
@@ -13,21 +17,23 @@ public class UglyNumbers {
            
         }
 		int x = 0;
-        while (n != 1) {
-            if (n % 5 == 0) {
-                n /= 5;
-            } else if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
+        while (n != 1) { //run code below if n is not equal to 1. 1 Is counted as a ugly number
+            if (n % 5 == 0) { //check if 5 is a prime factor of the number
+            	System.out.print("It is an ugly number.");
+            	break; //break the code
+            } else if (n % 3 == 0) { //check if 3 is a prime factor of the number
+            	System.out.print("It is an ugly number.");
+            	break; //break the code
+            } else if (n % 2 == 0) { //check if 2 is a prime factor of the number
+            	System.out.print("It is an ugly number.");
+            	break; //break the code
             } else {
                 System.out.print("It is not an ugly number.");
-				x = 1;
+                x = 1;
 				break;
             }
         }
-        if (x==0)
-		System.out.print("It is an ugly number.");
-		System.out.print("\n");
+       
+	
 	    }
 }
