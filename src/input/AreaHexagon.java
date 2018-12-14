f86edee5b434c7b24af86097efa61de84a32caa6
ivarnohre <ivar.nohre@elev.ga.ntig.se>
2018-12-14 09:23:07 +0100

    Update AreaHexagon.java

--- src/input/AreaHexagon.java
+++ src/input/AreaHexagon.java
@@ -3,7 +3,10 @@ package input;
 import java.util.Scanner;
 
 public class AreaHexagon {
-
+    /**
+     * This will calculate the area of a hexagon given the side length.
+     * @param args
+     */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter the side length of the hexagon: ");
@@ -12,8 +15,10 @@ public class AreaHexagon {
 		if (s <= 0) {
 			System.out.println("Invalid Number!");
 		} else {
-			double sSq = Math.pow(s, 2);
-			System.out.println("The Area is: " + (3 * Math.sqrt(3) / 2) * sSq);
+			double sSq = Math.pow(s, 2); //Side length to the power of 2, is used in the calculation.
+			System.out.println("The Area is: " + (3 * Math.sqrt(3) / 2) * sSq); //this calculation goes as:
+			//((3 times the power of 3) divided by 2) times the side length to the power of 2
+			
 
 		}
 
